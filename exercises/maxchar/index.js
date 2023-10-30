28d0a466f67593e32b0eff65068437acda970713
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-30 13:16:15 +0500

    Most Occuring Character (maxChar)

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  let maxChar = "";
+  let maxCount = 0;
+
+  for (const char of str) {
+    if(charMap[char]) {
+      charMap[char]++;
+    }
+    else {
+      charMap[char] = 1;
+    }
+
+    if(charMap[char] > maxCount) {
+      maxChar = char;
+      maxCount = charMap[char];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// Keep in mind:
+// ------------
+// - line 22:   if (charMap[char] > maxCount)
+//   we compare "charMap[char]" (count) to "maxCount" (count)
+//   NOT "charMap[char]" to "maxChar" (a string)
