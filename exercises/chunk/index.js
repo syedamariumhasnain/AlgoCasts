c981cd0c4701abef62542f49dcc77f2e3002060a
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-30 15:06:37 +0500

    Divide Array in Chunks of Arrays (chunk)

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,36 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // // Solution 1:   Slice Old Array & Push in New Array
+  // // -------------------------------------------------
+  let chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+
+  // // Solution 2:   Iterating each item & Push in Array
+  // // -------------------------------------------------
+  // let chunked = [];
+  // for (let i = 0; i <= array.length - 1; i++) {
+  //   let last = chunked[chunked.length - 1];
+  //   if (!last || last.length === size) {
+  //     chunked.push([array[i]]);
+  //   } else {
+  //     chunked[chunked.length - 1].push(array[i]);
+  //   }
+  // }
+  // return chunked;
+}
 
 module.exports = chunk;
+
+// Keep in mind:
+// ------------
+// - "size" is a size of chunk, NOT size of Array
+// - (index < array.length) OR (index <= array.length-1) is Correct
+//   (index < array.length-1) is NOT Correct
