04dacf306d0b9cf949bdc11984a04ad7c151fa98
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-30 12:34:55 +0500

    Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,31 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // // Solution 1:   STRING, ARRAY METHODS OF JAVASCRIPT
+  // // -------------------------------------------------
+  return str.split("").reverse().join("") === str;
+
+  // // Solution 2:   EVERY (LOOP & RETURN BOOLEAN)
+  // // -------------------------------------------
+  // return str.split("").every((char, i) => {
+  //   return char === str[str.length - i - 1];
+  // });
+
+  // // Solution 3:   TRADITIONAL FOR-LOOP (n/2 comparision)
+  // // ----------------------------------------------------
+  // for(let i=0; i <= (str.length-1)/2; i++) {
+  //   if(str[i] !== str[str.length -i -1]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+}
 
 module.exports = palindrome;
+
+// Keep in mind:
+// ------------
+// - Array func. "every" loops to check and return a boolean value 
+//   false if any value returns false, ONLY all trues return true
+// - Palindrome compares value of one end to the other end
