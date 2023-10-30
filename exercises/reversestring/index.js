70a029786b26937ca30d53483db1d3480e18b47b
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-30 12:02:52 +0500

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // // Solution 1:   STRING, ARRAY METHODS OF JAVASCRIPT
+  // // -------------------------------------------------
+  return str.split("").reverse().join("");
+
+
+  // // Solution 2:   FOR-OF LOOP (ARRAY)
+  // // ---------------------------------
+  // let reverse = "";
+  // for (const char of str) {
+  //   reverse = char + reverse;
+  // }
+  // return reverse;
+
+  
+  // // Solution 3:   TRADITIONAL FOR-LOOP
+  // // ----------------------------------
+  // let reverse = "";
+  // for(let i = str.length-1; i >= 0; i--) {
+  //   reverse += str[i];
+  // }
+  // return reverse;
+
+}
 
 module.exports = reverse;
