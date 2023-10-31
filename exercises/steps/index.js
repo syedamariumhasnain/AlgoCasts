89ec6f5127cfd00630ea6d493e01db30a1f8f03d
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-31 17:04:31 +0500

    Steps (+ Recursive Solution)

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+// // Solution 1:   STRING, ARRAY METHODS
+// // -----------------------------------
+for (let i = 1; i <= n; i++) {
+  console.log("#".repeat(i).concat(" ".repeat(n - i)));
+}
+
+// // Solution 2:   FOR-LOOP; Iterate each Character
+// // ----------------------------------------------
+// for (let row = 1; row <= n; row++) {
+//   let stair = "";
+//   for (let col = 1; col <= n; col++) {
+//     if (col <= row) {
+//       stair += "#";
+//     } else {
+//       stair += " ";
+//     }
+//   }
+//   console.log(stair);
+// }
+}
+
+// // Solution 3:   RECURSIVE FUNCTION
+// // --------------------------------
+// function steps(n, rows = 0, stair = "") {
+//   if (rows === n) {
+//     return;
+//   }
+//   if (stair.length === n) {
+//     console.log(stair);
+//     return steps(n, rows + 1);
+//   }
+
+//   const newStair = stair + (stair.length <= rows ? "#" : " ");
+//   steps(n, rows, newStair);
+// }
 
 module.exports = steps;
+
+// Keep in mind:
+// ------------
+// - RECURSIVE FUNCTION --- On Completing one stair:
+//   1- Show the Result it in Console...
+//   2- Return the new function to end the previous stair... 
