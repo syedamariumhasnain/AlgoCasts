4a56c0d4dc373884543538446f574496162694f2
syedamariumhasnain <syedamarium27@gmail.com>
2023-10-31 18:37:29 +0500

    Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // // Solution 1:   STRING, ARRAY METHODS
+  // // -----------------------------------
+  return str.split("").reduce((i, s) => {
+    return ["a", "e", "i", "o", "u"].includes(s.toLowerCase()) ? ++i : i;
+  }, 0);
+
+  // // Solution 2:   FOR-LOOP; Iterate each Character
+  // // ----------------------------------------------
+  // let count = 0;
+  // for (let char of str) {
+  //   ["a", "e", "i", "o", "u"].includes(char.toLowerCase()) && ++count;
+  // }
+  // return count;
+
+  // // Solution 3:   REGEX + match()
+  // // -----------------------------
+  // const matches = str.match(/[aeiou]/gi);
+  // return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// Keep in mind:
+// ------------
+// - Do ++i, NOT i++ just before returning i.e.  return ++i
+// - JS match() function returns items matched or null
