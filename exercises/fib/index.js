59c4288859e11502eb484ea7e7e89bafa8f04083
syedamariumhasnain <syedamarium27@gmail.com>
2023-11-01 01:03:17 -0700

    Fibonacci Series (Fib)

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  // // Solution 1:   FOR-LOOP; Iterate to nth element
+  // // ----------------------------------------------
+  // const series = [0, 1];
+  // for (let i = 2; i <= n; i++) {
+  //   series[i] = series[i - 2] + series[i - 1];
+  // }
+  // return series[series.length - 1];
+
+  // // Solution 1:   RECURSIVE SOLUTION
+  // // --------------------------------
+  if (n < 2) {
+    return n;
+  }
+  return fib(n-2) + fib(n-1);
+}
+
+// function memoize(fn) {
+//   const cache = {};
+
+//   return function(...args) {
+//     if(cache[args]) {
+//       return cache[args];
+//     }
+
+//     const res = fn.apply(this, args);
+//     cache[args] = res;
+//     return res;
+//   }
+// }
+
+// function slowFib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n-2) + fib(n-1);
+// }
+
+// const fib = memoize(slowFib);
+
+
 
 module.exports = fib;
