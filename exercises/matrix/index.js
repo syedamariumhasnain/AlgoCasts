0c96f0a6db26d80a0a44fb360a7fbe4672fb7aab
syedamariumhasnain <syedamarium27@gmail.com>
2023-11-01 00:22:04 -0700

    Spiral Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,56 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let count = 1;
+    let startRow = 0;
+    let endRow = n-1;
+    let startColumn = 0;
+    let endColumn = n-1;
+  
+    const matrix = [];
+    for(let i=0; i<n; i++) {
+      matrix.push([]);
+    }
+  
+    while(startRow <= endRow && startColumn <= endColumn) {
+      // Write Start Row
+      for(let col=startColumn; col <=endColumn; col++) {
+        matrix[startRow][col] = count;
+        ++count;
+      }
+      ++startRow;
+  
+      // Write End Column
+      for(let row=startRow; row <=endRow; row++) {
+        matrix[row][endColumn] = count;
+        ++count;
+      }
+      --endColumn
+  
+      // Write End Row
+      for(let col=endColumn; col >= startColumn; col--) {
+        matrix[endRow][col] = count;
+        ++count;
+      }
+      --endRow;
+  
+      // Write Start Column
+      for(let row=endRow; row >=startRow; row--){
+        matrix[row][startColumn] = count;
+        ++count
+      }
+      ++startColumn
+    }
+    console.log(matrix);
+    return matrix;
+  }
 
 module.exports = matrix;
+
+// Keep in mind:
+// ------------
+// - Iterate startColumn to endColumn to fill Start Row
+//   Iterate startRow to endRow to fill End Column
+//   Iterate endColumn to startColumn to fill End Row
+//   Iterate endRow to startRow to fill Start Column
